88cf95ef1c0694508d6967704ee15b2811f99453
josh <josh.zhu@pictureworks.biz>
2018-02-02 14:54:41 +0800

    add interval

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require('./global.js')
 const faceai = require('./faceai.js')
 
-async function faceProcess() {
+async function main() {
 	let photoCount = await model.photo.count()
 	log(`total has ${photoCount} photos`)
 	const photos = await model.photo
@@ -29,45 +29,43 @@ async function faceProcess() {
 			}, {
 				multi: true
 			}).exec()
-		}
-		for (key of ary) {
-			log(key)
-			const face = new model.face()
-			face.name = key
-			const rs = await face.save()
-			log(rs)
-			await model.photo.update({
-				rawFileName: photo.rawFileName,
-				'faces.0': {
-					$exists: true
-				}
-			}, {
-				$addToSet: {
-					faces: rs._id.toString()
-				}
-			}, {
-				multi: true
-			}).exec()
-			await model.photo.update({
-				rawFileName: photo.rawFileName,
-				'faces.0': {
-					$exists: false
-				}
-			}, {
-				$set: {
-					faces: [rs._id.toString()]
-				}
-			}, {
-				multi: true
-			}).exec()
+		} else {
+			for (key of ary) {
+				log(key)
+				const face = new model.face()
+				face.name = key
+				const rs = await face.save()
+				log(rs)
+				await model.photo.update({
+					rawFileName: photo.rawFileName,
+					'faces.0': {
+						$exists: true
+					}
+				}, {
+					$addToSet: {
+						faces: rs._id.toString()
+					}
+				}, {
+					multi: true
+				}).exec()
+				await model.photo.update({
+					rawFileName: photo.rawFileName,
+					'faces.0': {
+						$exists: false
+					}
+				}, {
+					$set: {
+						faces: [rs._id.toString()]
+					}
+				}, {
+					multi: true
+				}).exec()
+			}
 		}
 	}
-}
-
-async function main() {
-	setInterval(async _ => {
-		await faceProcess()
-	}, 2000)
+	log('====================================')
+	awiat Promise.delay(2000)
+	await main()
 }
 
 main()
