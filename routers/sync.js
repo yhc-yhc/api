59fe37966fdc6cf90618175990ef0bff39b58994
josh <josh.zhu@pictureworks.biz>
2018-03-28 16:20:27 +0800

    add sync

---
+++ routers/sync.js
@@ -0,0 +1,26 @@
+const Router = require('koa-router')
+const router = new Router()
+
+router.get('listCards', async(ctx, next) => {
+	const user = await model.user.findOne({
+		_id: ctx.user.userid
+	})
+	const codes = user.customerIds.map(obj => obj.code)
+	const photos = await model.photo.find({
+		'customerIds.code': {
+			$in: codes
+		}
+	}, {
+		_id: 0,
+		thumbnail: 1,
+		original: 1,
+		orderHistory: 1,
+		siteId: 1,
+		'customerIds.code': 1,
+		shootOn: 1
+	})
+	const cards = await services.photo.photosToCards(photos, codes)
+	ctx.body = cards
+})
+
+module.exports = router
