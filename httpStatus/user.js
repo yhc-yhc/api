0f537ba342d5ed9d6827cde8852f1dd8ea9ee5d8
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-10 14:13:57 +0800

    weixin app login 0002

--- httpStatus/user.js
+++ httpStatus/user.js
@@ -5,5 +5,17 @@ module.exports = {
 		headers: {},
 		params: {},
 		resDesc: {}
-	}
+	},
+	wxlogin: {
+        method: 'POST',
+        desc: '微信登录app并获取用户信息',
+        headers: {
+			// token: [1, 'String', "标识用户身份"]
+		},
+		params: {
+			access_token: [1, 'String', "微信登录认证的token"],
+			openid: [1, 'String', "app认证的openid，授权用户唯一标识"],
+			
+		}
+    }
 }
