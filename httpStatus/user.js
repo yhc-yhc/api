10b65d30c372b0e228fd393f5d0bf7d4655a561b
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-16 09:43:41 +0800

    wx final 002

--- httpStatus/user.js
+++ httpStatus/user.js
@@ -7,19 +7,22 @@ module.exports = {
 		resDesc: {}
 	},
 	thirdLogin: {
-        method: 'POST',
-        desc: '微信登录app并获取用户信息',
-        headers: {
-			 uuid: [1, 'String', "设备的uuid"]
-		},
+		method: 'POST',
+		desc: '微信登录app并获取用户信息',
 		params: {
-			// access_token: [1, 'String', "微信登录认证的token"],
-			// openid: [1, 'String', "app认证的openid，授权用户唯一标识"],
-			type:[1, 'String', "wx/fb"],
-			terminal:[1, 'String', "ios/adriod"]
+			access_token: [0, 'String', "微信登录认证的token"],
+			openid: [0, 'String', "app认证的openid，授权用户唯一标识"],
+			uuid: [1, 'String', "设备的uuid"],
+			type: [1, 'String', "wx/fb"],
+			terminal: [1, 'String', "ios/adriod"]
 		},
-		resDesc:{
+		response: [{
+			token: '2bd415d0c0e3a096d64e26f'
+
+		}],
+
+		resDesc: {
 			'token': ['String', '认证标志'],
 		}
-    }
+	}
 }
