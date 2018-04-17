d8c9de53a5afebf1b23203fb01fda90c4595c764
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-17 12:08:05 +0800

    user.js type

--- services/user.js
+++ services/user.js
@@ -1,31 +1,32 @@
 const jwt = require('jsonwebtoken');
 const uuid = require('uuid')
 
-exports.createToken = async (user, ctx) => {
+exports.createToken = async (userName, params) => {
 	if (!certs.private) {
 		await loadCert()
 	}
-	log('获取到的ip: '+ctx.request.ip.replace(/::ffff:/g, ''))
 	var token = {
 		iat: Math.floor(Date.now() / 1000),
 		exp: Math.floor(Date.now() / 1000) + 604800,
 		iss: 'pictureAir',
-		uuid: ctx.params.uuid,
-		visitIP: ctx.request.ip.replace(/::ffff:/g, ''),
-		audience: endeurl.md5(user.userName), //md5(user)
-		t: ctx.params.terminal, //web photo
+		uuid: params.uuid,
+		visitIP: params.visitIP,
+		audience: endeurl.md5(userName), //md5(user)
+		t: params.t, //web photo
 		appid: '',
-		lg: ctx.params.lg
+		lg: params.lg
 	}
 	return jwt.sign(token, certs.private, {
 		algorithm: 'RS512'
 	})
 }
 
-exports.wxLogin = async ctx => {
-	const questString = `access_token=${ctx.params.access_token}&openid=${ctx.params.openid}`
+exports.wxLogin = async loginparams => {
+
+
+	const questString = `access_token=${loginparams.token}&openid=${loginparams.openid}`
 	const loginUrl = `https://api.weixin.qq.com/sns/auth?${questString}`
-	const infoUrl = 'https://api.weixin.qq.com/sns/userinfo?' + questString
+	const infoUrl = `https://api.weixin.qq.com/sns/userinfo?${questString}` 
 
 	const validateLogin = await request.getAsync({
 		url: loginUrl
@@ -34,8 +35,8 @@ exports.wxLogin = async ctx => {
 	if (isLogin.errcode != 0 && isLogin.errmsg != 'ok') {
 		throw {
 			status: 10003,
-			message: httpStatus.common.system['10003'][ctx.LG],
-			router: ctx.url
+			message: httpStatus.common.system['10003'][loginparams.LG],
+			router: loginparams._url
 		}
 	}
 	const userInfoRes = await request.getAsync({
@@ -44,27 +45,23 @@ exports.wxLogin = async ctx => {
 	if (userInfoRes.statusCode != 200) {
 		throw {
 			status: 10003,
-			message: httpStatus.common.system['10003'][ctx.LG],
-			router: ctx.url
+			message: httpStatus.common.system['10003'][loginparams.LG],
+			router: loginparams._url
 		}
 	}
 	const userInfo = JSON.parse(userInfoRes.body)
-	// const addressArr = []
-	// addressArr[0] = ''
-	// addressArr[1] = userInfo.province
-	// addressArr[2] = userInfo.city
-	// addressArr[3] = userInfo.country
 	const PPCode = "PWUP" + mongoose.Types.ObjectId().toString().substr(12, 12).toUpperCase()
 	const user = {
 		userName: userInfo.nickname,
 		name: userInfo.nickname,
-		openIds: {wx:userInfo.openid},
+		openIds: {
+			wx: userInfo.openid
+		},
 		gender: userInfo.sex,
 		country: userInfo.country,
-		// addresses: addressArr,
 		coverHeaderImage: userInfo.headimgurl,
 		unionid: userInfo.unionid, //用户的unionid是唯一的。换句话说，同一用户，对同一个微信开放平台下的不同应用，unionid是相同的
-		registerTerminal: ctx.params.terminal, //终端类型ios,adriod
+		registerTerminal: loginparams.terminal, //终端类型ios,adriod
 		creDatetime: new Date(),
 		updDatetime: new Date(),
 		userPP: PPCode,
@@ -75,19 +72,19 @@ exports.wxLogin = async ctx => {
 	return user
 }
 
-exports.fbLogin = async ctx => {
-	
+exports.fbLogin = async loginparams => {
+
 	//.利用token获取用户基本信息
-    const fb_token = ctx.params.access_token
-    const fb_query='fields=link,id,name,picture,first_name,gender&'
+	const fb_token = loginparams.token
+	const fb_query = 'fields=link,id,name,picture,first_name,gender&'
 	const user_res = await request.getAsync({
 		url: `https://graph.facebook.com/me?${fb_query}access_token=${fb_token}`
 	})
-	if (user_res.statusCode!=200 && !user_res.body) {
+	if (user_res.statusCode != 200) {
 		throw {
 			status: 10003,
-			message: httpStatus.common.system['10003'][ctx.LG],
-			router: ctx.url
+			message: httpStatus.common.system['10003'][loginparams.LG],
+			router: loginparams._url
 		}
 	}
 	const user_info = JSON.parse(user_res.body)
@@ -101,7 +98,7 @@ exports.fbLogin = async ctx => {
 		coverHeaderImage: userInfo.picture.data.url,
 		gender: user_info.gender,
 		country: user_info.locale,
-		registerTerminal: ctx.params.terminal, //终端类型ios,adriod
+		registerTerminal: loginparams.terminal, //终端类型ios,adriod
 		creDatetime: new Date(),
 		updDatetime: new Date(),
 		userPP: PPCode,
