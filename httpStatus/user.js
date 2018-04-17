7cc951e23fb1a34e92ebef53982e1596f93bc632
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-17 12:07:49 +0800

    user.js type

--- httpStatus/user.js
+++ httpStatus/user.js
@@ -8,18 +8,18 @@ module.exports = {
 	},
 	thirdLogin: {
 		method: 'POST',
-		desc: '微信登录app并获取用户信息',
+		desc: '第三方登录',
 		headers: {},
 		params: {
-			access_token: [0, 'String', "微信登录认证的token"],
-			openid: [0, 'String', "app认证的openid，授权用户唯一标识"],
+			access_token: [1, 'String', "第三方登录认证的token"],
+			openid: [0, 'String', "app认证的openid，授权用户唯一标识,微信为必需"],
 			uuid: [1, 'String', "设备的uuid"],
 			type: [1, 'String', "wx/fb"],
-			terminal: [1, 'String', "ios/adriod"],
-			lg:[1,'String','en/us']
+			terminal: [1, 'String', "ios/adriod,按照之前的0或1传"],
+			lg: [1, 'String', 'en/us']
 		},
 		response: {
-			token:'2bd415d0c0e3a096d64e26f'
+			token: '2bd415d0c0e3a096d64e26f'
 		},
 		resDesc: {
 			'token': ['String', '认证标志'],
