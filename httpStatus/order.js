dc3d3d39b273aeeac26ae748274ca333a1e890ed
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-21 16:01:44 +0800

    orderquery  update 002

--- httpStatus/order.js
+++ httpStatus/order.js
@@ -5,7 +5,7 @@ module.exports = {
 		headers: {},
 		params: {
 			isPay:[1,'boolean',"订单状态是否交易成功"],
-			orderCode:[0,'String',"订单号"]
+			createdBy:[0,'String',"当前用户的userid"]
 		},
 		resDesc: {}
 	}
--- routers/order.js
+++ routers/order.js
@@ -1,9 +1,10 @@
 const router = new Router()
-const services = loaddir('services')
+const services = loaddir('./services')
 
 //1等待买家付款，2买家正在付款，3买家已付款（等待卖家发货），4卖家已发货（等待买家确认），5交易成功，6交易关闭,订单冻结, 7退款中，8退款成功
 //3,5都是已完成，其他的都是未完成
 router.get('getOrdersISpay', async (ctx, next) => {
+
 	const ispayFlag = ctx.params.isPay
 	let payStatus
 	if (!ispayFlag) {
@@ -16,10 +17,8 @@ router.get('getOrdersISpay', async (ctx, next) => {
 		'orderStatus.status': {
 			$in: payStatus
 		},
-		isDelete: false
-	}
-	if(ctx.params.orderCode && ctx.params.orderCode != ''){
-         querParam.orderCode = ctx.params.orderCode
+		isDelete: false,
+		createdBy:ctx.user.userid
 	}
 	const orderInfo = await model.order.find(querParam,{},{sort: { _id: -1 }})
 	if(!orderInfo){
