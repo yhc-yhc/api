9902a3421dbc5509b8b335384f698fe3165a1fe3
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-23 10:09:33 +0800

    orderquery  update 004

--- httpStatus/order.js
+++ httpStatus/order.js
@@ -1,13 +1,33 @@
 module.exports = {
-	getOrdersISpay: {
+	getOrdersStatus: {
 		method: 'GET',
 		desc: "查询订单是否交易成功",
 		headers: {
-			token: [1, 'String', "标识用户身份"]
+			token: [1, 'String', '用户认证id']
 		},
 		params: {
-			isPay:[1,'boolean',"订单状态是否交易成功"]
+			isPay: [1, 'boolean', "订单状态是否交易成功"]
+
 		},
-		resDesc: {}
+		response: {
+			"orders": [{
+				"orderCode": "1103170331623587",
+				"createdOn": "2017-03-31 14:29:08",
+				"couponsList": [],
+				"orderStatus": 1,
+				"totalPrice": 3000,
+				"discount": 1000,
+				"express": "1",
+				"resultPrice": 4000,
+				"entity": [],
+				"cardUrl": "/sites/RSSG/orderCard.png",
+				"virtualProducts": [{
+					"productType": 0,
+					"count": 1,
+					"currency": "JYP",
+					"parkName": "LEGOLAND®️ Japan"
+				}]
+			}]
+		}
 	}
 }
--- routers/order.js
+++ routers/order.js
@@ -3,9 +3,9 @@ const services = loaddir('./services')
 
 //1等待买家付款，2买家正在付款，3买家已付款（等待卖家发货），4卖家已发货（等待买家确认），5交易成功，6交易关闭,订单冻结, 7退款中，8退款成功
 //3,5都是已完成，其他的都是未完成
-router.get('getOrdersISpay', async (ctx, next) => {
+router.get('getOrdersStatus', async (ctx, next) => {
 	let payStatus
-	if (ctx.params.isPay) {
+	if (ctx.params.isPay == 'true') {
 		payStatus = [3, 5]
 	} else {
 		payStatus = [1, 2, 4, 6, 7, 8]
