ec19e31d5b4068d38baa2cd850be39daa5c15872
eggsy.cao <eggsy.cao@pictureworks.biz>
2018-04-23 09:18:04 +0800

    orderquery  update 004

--- routers/order.js
+++ routers/order.js
@@ -4,29 +4,45 @@ const services = loaddir('./services')
 //1等待买家付款，2买家正在付款，3买家已付款（等待卖家发货），4卖家已发货（等待买家确认），5交易成功，6交易关闭,订单冻结, 7退款中，8退款成功
 //3,5都是已完成，其他的都是未完成
 router.get('getOrdersISpay', async (ctx, next) => {
-	const ispayFlag = ctx.params.isPay
 	let payStatus
-	if (!ispayFlag) {
-		payStatus = [1, 2, 4, 6, 7, 8]
-	} else {
+	if (ctx.params.isPay) {
 		payStatus = [3, 5]
+	} else {
+		payStatus = [1, 2, 4, 6, 7, 8]
 	}
-	log('payStatus: '+payStatus)
+	log('payStatus: ' + payStatus)
 	const querParam = {
 		'orderStatus.status': {
 			$in: payStatus
 		},
 		isDelete: false,
-		createdBy:ctx.user.userid
+		createdBy: ctx.user.userid
 	}
-	const orderInfo = await model.order.find(querParam,{},{sort: { _id: -1 }})
-	if(!orderInfo){
-        throw {
-			message: 'orderInfo not exists',
-			router:ctx_url
+	const orderInfo = await model.order.find(querParam).sort({
+		_id: -1
+	})
+	let orders = []
+	if (!orderInfo.length) {
+		ctx.body = {
+			orders
 		}
+		return
 	}
-	ctx.body = {orderInfo}
-
+	const siteId = orderInfo.map(order => order.siteId)
+	const park = await model.park.find({
+		siteId: {
+			$in: siteId
+		}
+	})
+	const parkMap = park.reduce((pre, cur) => {
+		pre[cur.siteId] = cur
+		return pre
+	}, {})
+	for (let order of orderInfo) {
+		orders.push(services.order.orderFomat(order, parkMap[order.siteId]))
+	}
+	ctx.body = {
+		orders
+	}
 })
 module.exports = router
---
+++ services/order.js
@@ -0,0 +1,49 @@
+function getOrderStatus(status) {
+	switch (true) {
+		case /[1]/.test(status):
+			return 1 //未付款
+			break
+		case /[2,7,10]/.test(status):
+			return 2 //处理中
+			break
+		case /[3,5]/.test(status):
+			return 3 //已完成
+			break
+		case /[6]/.test(status):
+			return 4 //已取消
+			break
+		case /[8]/.test(status):
+			return 5 //已退款
+			break
+		case /[9]/.test(status):
+			return 6 //交易失败
+			break
+	}
+}
+exports.orderFomat = (order, park) => {
+	const obj = {}
+	obj.orderCode = order.orderCode
+	obj.transCode = order.transCode
+	obj.createdOn = order.createdOn
+	obj.couponsList = order.couponsList
+	obj.orderStatus = getOrderStatus(order.orderStatus.status)
+	obj.totalPrice = order.orderProducts.resume.totalPrice.toString()
+	obj.currency = order.currency
+	obj.discount = order.orderProducts.resume.preferentialPrice.toString()
+	obj.express = order.orderProducts.resume.express.toString()
+	obj.resultPrice = order.orderProducts.resume.resultPrice.toString()
+	//obj.orderProducts = new FilterOrderProductsToRes(order.orderProducts, park)
+	obj.entity = order.orderProducts.entity
+	obj.cardUrl = park.cardUrl //'/sites/common/orderCard.png'
+	obj.virtualProducts = []
+	for (let item of order.orderProducts.virtualProducts) {
+		obj.virtualProducts.push({
+			productType: item.productType,
+			count: item.count,
+			currency: park.currency || '',
+			parkName: park.parkName || ''
+		})
+
+	}
+	return obj
+}
